bb7eb91c610dddbfeaf3947b23fa3fc1180e1a72
Felipe Morais Aragão <Felipe_link64@hotmail.com>
2020-01-08 22:40:21 -0300

    Union Find

---
+++ Algorithms/Collections/UnionFind.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Collections
+{
+    public class UnionFind
+    {
+        private readonly int[] _vector;
+        
+        /// <summary>
+        /// Counts the amount of sets in the collection.
+        /// </summary>
+        public int Count { get; private set; }
+
+        public UnionFind(int capacity)
+        {
+            _vector = new int[capacity];
+            Count = capacity;
+
+            for (int i = 0; i < capacity; i++)
+                _vector[i] = i;
+        }
+
+        private int FindRoot(int q)
+        {
+            while (q != _vector[q])
+                q = _vector[q];
+
+            return q;
+        }
+
+        public void Union(int q, int p)
+        {
+            int i = FindRoot(p);
+            int j = FindRoot(q);
+            _vector[i] = j;
+        }
+
+        public bool IsConnected(int p, int q) => FindRoot(p) == FindRoot(q);
+    }
+}
