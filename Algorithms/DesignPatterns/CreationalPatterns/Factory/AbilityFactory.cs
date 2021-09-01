ef56dd647d14810e13399e2737efb565584fb37f
felipemsx <felipemsx18@gmail.com>
2021-08-31 22:03:28 -0300

    factory pattern in progress

---
+++ Algorithms/DesignPatterns/CreationalPatterns/Factory/AbilityFactory.cs
@@ -0,0 +1,12 @@
+﻿using Algorithms.DesignPatterns.CreationalPatterns.Factory;
+
+namespace Algorithms.DesignPatterns.CreationalPatterns.Factory
+{
+    public class AbilityFactory : AssemblyFactoryBase<Ability>
+    {
+        public AbilityFactory()
+        {
+            base.Initialize(nameof(NullableAbility));
+        }
+    }
+}
