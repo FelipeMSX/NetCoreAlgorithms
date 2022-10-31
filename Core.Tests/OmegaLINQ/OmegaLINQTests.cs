96e8ff03129fd07407af8b17ece5958f7259e571
FelipeMSX <felipemsx18@gmail.com>
2022-10-31 08:15:52 -0300

    Dependencies in Algorithms has been changed

---
+++ Core.Tests/OmegaLINQ/OmegaLINQTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Core.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Tests.Iterators
+{
+    [TestClass]
+    public class OmegaLINQTests
+    {
+
+        private IOmegaList<int?> _list;
+
+        [TestInitialize]
+        public void TearUp()
+        {
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _list = null;
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+
+    }
+}
+
