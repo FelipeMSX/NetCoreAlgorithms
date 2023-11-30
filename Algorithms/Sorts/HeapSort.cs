3c087dea82ba846d27b65855ec7c29d10a86b613
FelipeMSX <felipemsx18@gmail.com>
2023-11-30 18:22:44 -0300

    heap sort adjusted

--- Algorithms/Sorts/HeapSort.cs
+++ Algorithms/Sorts/HeapSort.cs
@@ -4,6 +4,8 @@ using Algorithms.Helpers;
 using System.Collections.Generic;
 using OmegaCore.Collections.Interfaces;
 using OmegaCore.OmegaLINQ;
+using OmegaCore.Exceptions;
+using System;
 
 namespace Algorithms.Sorts
 {
@@ -21,21 +23,14 @@ namespace Algorithms.Sorts
         /// </summary>
         public Build Operation { get; }
 
-        private readonly IHeapSortBuildStrategy<T> _buildStrategy;
-
-        #region Constructors
         /// <param name="comparator">Defines a simple way to compare two objects</param>
         /// <param name="build">The heap can be building using MIN or MAX strategy</param>
-        public HeapSort(OmegaComparison<T> comparator, Build build)
+        public HeapSort(OmegaComparison<T> comparator, Build build = Build.Max)
         {
             Comparator = comparator;
             Operation = build;
         }
 
-        /// <param name="comparator">Defines a simple way to compare two objects</param>
-        public HeapSort(OmegaComparison<T> comparator) : this(comparator, Build.Max) { }
-
-        #endregion
 
         /// <summary>
         /// Orders the collection using the heapsort algorithm.
@@ -47,11 +42,11 @@ namespace Algorithms.Sorts
         /// <exception cref="NullObjectException"/>
         public void Sort(IOmegaList<T> list)
         {
-            //validações
+            //Validations
+            OmegaCore.Exceptions.ArgumentNullException.CheckAgainstNull(list, nameof(list));
+
             if (Comparator == null)
                 throw new ComparatorNotSetException();
-            if (list == null)
-                throw new NullObjectException();
 
             Heapsort(list);
         }
@@ -79,38 +74,20 @@ namespace Algorithms.Sorts
             int leftPosition = Left(currentPosition);
             int rightPosition = Right(currentPosition);
 
-            PositionData position = new()
-            {
-                Current = currentPosition,
-                Left = leftPosition,
-                Right = rightPosition,
-                ListLength = listLength,
-            };
+            // leftPosition < position. Verifica se não extrapola os limites do vetor.
+            // Se o filho esquerdo for maior que o pai é necessário trocar.
+            if (leftPosition < listLength && CheckCompareResult(Comparator(list[leftPosition], list[currentPosition])))
+                Realocate(list, currentPosition, leftPosition, listLength);
 
-            if (Operation == Build.Min)
-            {
-                // leftPosition < position. Verifica se não extrapola os limites do vetor.
-                // Se o filho esquerdo for maior que o pai é necessário trocar.
-                if (leftPosition < listLength && Comparator(list[leftPosition], list[currentPosition]) <= 0)
-                    Realocate(list, currentPosition, leftPosition, listLength);
+            // rightPosition < position. Verifica se não extrapola os limites do vetor.
+            // Se o filho direito for maior que o pai é necessário trocar.
+            if (rightPosition < listLength && CheckCompareResult(Comparator(list[leftPosition], list[currentPosition])))
+                Realocate(list, currentPosition, rightPosition, listLength);
 
-                // rightPosition < position. Verifica se não extrapola os limites do vetor.
-                // Se o filho direito for maior que o pai é necessário trocar.
-                if (rightPosition < listLength && Comparator(list[rightPosition], list[currentPosition]) <= 0)
-                    Realocate(list, currentPosition, rightPosition, listLength);
-            }
-            else
-            {
-                // leftPosition > position. Verifica se não extrapola os limites do vetor.
-                // Se o filho esquerdo for menor que o pai é necessário trocar.
-                if (leftPosition < listLength && Comparator(list[leftPosition], list[currentPosition]) >= 0)
-                    Realocate(list, currentPosition, leftPosition, listLength);
-
-                // rightPosition > position. Verifica se não extrapola os limites do vetor.
-                // Se o filho direito for maior que o pai é necessário trocar.
-                if (rightPosition < listLength && Comparator(list[rightPosition], list[currentPosition]) >= 0)
-                    Realocate(list, currentPosition, rightPosition, listLength);
-            }
+        }
+        private bool CheckCompareResult(int compareResult)
+        {
+            return Operation == Build.Min ? compareResult <= 0 : compareResult >= 0;
         }
 
         /// <summary>
@@ -131,101 +108,25 @@ namespace Algorithms.Sorts
                 Heapify(list, i, listLength);
         }
 
+        /// <summary>
+        /// Based on the position calculates his left son.
+        /// </summary>
+        /// <returns>Returns the position of the left son in the array</returns>
         private static int Left(int i)
         {
             return 2 * i + 1;
         }
 
-        private static int Right(int i)
+        /// <summary>
+        /// Based on the position calculates his right son.
+        /// </summary>
+        /// <returns>Returns the position of the right son in the array</returns>
+        private static int Right(int position)
         {
-            return 2 * i + 2;
-        }
-
-        public void teste()
-        {
-
-        }
-
-
-
-
-    }
-
-
-    public readonly struct PositionData
-    {
-        public required int Current { get; init; }
-        public required int Left { get; init; }
-        public required int Right { get; init; }
-        public required int ListLength { get; init; }
-    }
-
-    public interface IHeapSortBuildStrategy<T>
-    {
-        Build Build { get; }
-        bool CheckLeft(IOmegaList<T> list, OmegaComparison<T> comparator, PositionData positionData);
-        bool CheckRight(IOmegaList<T> list, OmegaComparison<T> comparator, PositionData positionData);
-    }
-
-    public class HeapSortBuildMinStrategy<T> : IHeapSortBuildStrategy<T>
-    {
-        public Build Build { get; private set; }
-
-        public HeapSortBuildMinStrategy()
-        {
-            Build = Build.Min;
-
-
-        }
-
-        //        if (Operation == Build.Min)
-        //{
-        //    // leftPosition < position. Verifica se não extrapola os limites do vetor.
-        //    // Se o filho esquerdo for maior que o pai é necessário trocar.
-        //    if (leftPosition<listLength && Comparator(list[leftPosition], list[currentPosition]) <= 0)
-        //        Realocate(list, currentPosition, leftPosition, listLength);
-
-        //    // rightPosition < position. Verifica se não extrapola os limites do vetor.
-        //    // Se o filho direito for maior que o pai é necessário trocar.
-        //    if (rightPosition<listLength && Comparator(list[rightPosition], list[currentPosition]) <= 0)
-        //        Realocate(list, currentPosition, rightPosition, listLength);
-        //}
-
-        public bool CheckLeft(IOmegaList<T> list, OmegaComparison<T> comparator, PositionData positionData)
-        {
-            bool boundaryCheck = positionData.Left < positionData.ListLength;
-            bool checkLeftAgainstCurrent = comparator(list[positionData.Left], list[positionData.Current]) <= 0;
-            return boundaryCheck && checkLeftAgainstCurrent;
-        }
-
-        public bool CheckRight(IOmegaList<T> list, OmegaComparison<T> comparator, PositionData positionData)
-        {
-            throw new System.NotImplementedException();
+            return 2 * position + 2;
         }
     }
 
-    public class HeapSortBuildMaxStrategy<T> 
-    {
-        public Build Build { get; }
-
-        public HeapSortBuildMaxStrategy()
-        {
-            Build = Build.Max;
-        }
-
-        public bool CheckLeft(IOmegaList<T> list, int currentPosition, int leftPosition, int listLength)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public bool CheckRight(IOmegaList<T> list, int currentPosition, int rightPosition, int listLength)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
-
-
-    ///// </summary>
     /// <summary>
     /// Indicates whether the heap will be build using the MIN or MAX strategy.
     /// </summary>
